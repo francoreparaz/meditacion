89dda93cb39f8c72a17bca497c0f5044f7550441
Franco Reparaz Fiori <66220845+francoreparaz@users.noreply.github.com>
2021-04-13 12:26:26 -0300

    12:26

--- src/PersonalDeSalud.js
+++ src/PersonalDeSalud.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
+import {Form,Button} from "react-bootstrap"
+import "./PersonalDeSalud.css"
+import { Checkmark } from 'react-checkmark'
 
 export const PersonalDeSalud = () => {
-    return (
-        <div>
-             <h1>Personal De Salud</h1>
-             <form className="form">
-                 <input type="text" placeholder="Ingresá tu nombre"></input>
-                 <input type="text" placeholder="Ingresá tu mail"></input>
-                 <input type="password" placeholder="Ingresá tu contraseña"></input>
-                 <input type="submit"></input>
-             </form>
-        </div>
-    )
-}
- export default PersonalDeSalud
+  const handleSubmit=(e)=>{
+   e.preventDefault()
+   
+  }
+  return (
+    <div>
+      <h1>Personal De Salud</h1>
+      <Form className="form" onSubmit={handleSubmit}>
+        <h1 className="registro"><strong>Registrate</strong></h1>
+        <Form.Group controlId="formBasicEmail" >
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword" className="password">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit" className="button">
+          Submit
+        </Button>
+      
+      </Form>
+    </div>
+  );
+};
+export default PersonalDeSalud;
